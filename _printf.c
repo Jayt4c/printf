9422fa3f4b0403e1cc7d2f357fefba2a2ace0f4e
Jayt4c <igejuwon53@gmail.com>
2023-11-16 04:55:36 -0800

    changed printf functions

---
+++ _printf.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
+
+/**
+ * _printf - printf function
+ * @format: Format string
+ * @...: More arguments
+ * Return: Number of characters printed
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	int count = 0;
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					{
+						int value = va_arg(args, int);
+
+						count += write(1, &value, 1);
+
+					}
+					break;
+				case 's':
+					count += write(1, va_arg(args, char *), 1);
+					break;
+				case '%':
+					count += write(1, "%", 1);
+					break;
+				case 'd':
+				case 'i':
+					count += print_int(va_arg(args, int));
+					break;
+				default:
+					count += write(1, "%", 1);
+					count += write(1, format, 1);
+					break;
+			}
+		}
+		else
+		{
+			count += write(1, format, 1);
+		}
+		format++;
+	}
+
+	va_end(args);
+
+	return (count);
+}
+
