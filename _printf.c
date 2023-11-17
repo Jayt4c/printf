beb6c3c2cc951ebbd12c3e1f5c9efa8d47856167
Jayt4c <igejuwon53@gmail.com>
2023-11-17 10:08:09 -0800

    _printf betty solved

--- _printf.c
+++ _printf.c
@@ -25,48 +25,40 @@ int _printf(const char *format, ...)
 			format++;
 			switch (*format)
 			{
-				case 'c':
-					{
-						int value = va_arg(args, int);
+			case 'c':
+			{
+				int value = va_arg(args, int);
 
-						count += write(1, &value, 1);
+				count += write(1, &value, 1);
 
-					}
-					break;
-				case 's':
-					count += write(1, va_arg(args, char *), 1);
-					break;
-				case '%':
-					count += write(1, "%", 1);
-					break;
-				case 'd':
-				case 'i':
-					count += print_int(va_arg(args, int));
-					break;
-				case 'u':
-					{
-						count += print_unsigned(args);
-					}
-					break;
-				case 'o':
-					{
-						count += print_octal(args);
-					}
-					break;
-				case 'x':
-					{
-						count += print_hexa(args, 0);
-					}
-					break;
-				case 'X':
-					{
-						count += print_hexa(args, 1);
-					}
-					break;
-				default:
-					count += write(1, "%", 1);
-					count += write(1, format, 1);
-					break;
+			}
+			break;
+			case 's':
+				count += write(1, va_arg(args, char *), 1);
+				break;
+			case '%':
+				count += write(1, "%", 1);
+				break;
+			case 'd':
+			case 'i':
+				count += print_int(va_arg(args, int));
+				break;
+			case 'u':
+				count += print_unsigned(args);
+				break;
+			case 'o':
+				count += print_octal(args);
+				break;
+			case 'x':
+				count += print_hexa(args, 0);
+				break;
+			case 'X':
+				count += print_hexa(args, 1);
+				break;
+			default:
+				count += write(1, "%", 1);
+				count += write(1, format, 1);
+				break;
 			}
 		}
 		else
--- conversions.c
+++ conversions.c
@@ -1,57 +1,53 @@
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * _printf - char functions
+ *
+ * Return: return buffer
+ */
 
-/** Function to convert unsigned integer to string */
-char *uint_to_str(unsigned int value) {
-    /* Determine the maximum number of digits required */
-    int num_digits = snprintf(NULL, 0, "%u", value);
+char *uint_to_str(unsigned int value)
+{
+	int num_digits = snprintf(NULL, 0, "%u", value);
+	char *buffer = (char *)malloc(num_digits + 1);
 
-    /* Allocate memory for the string, including space for the null terminator */
-    char *buffer = (char *)malloc(num_digits + 1);
-    if (buffer == NULL) {
-        /* Handle memory allocation failure */
-        exit(EXIT_FAILURE);
-    }
+	if (buffer == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
 
-    /* Convert the unsigned integer to string */
-    sprintf(buffer, "%u", value);
+	sprintf(buffer, "%u", value);
 
-    return buffer;
+	return (buffer);
 }
 
-/** Function to convert unsigned integer to octal string */
-char *uint_to_octal_str(unsigned int value) {
-    /* Determine the maximum number of digits required */
-    int num_digits = snprintf(NULL, 0, "%o", value);
+char *uint_to_octal_str(unsigned int value)
+{
+	int num_digits = snprintf(NULL, 0, "%o", value);
+	char *buffer = (char *)malloc(num_digits + 1);
 
-    /* Allocate memory for the string, including space for the null terminator */
-    char *buffer = (char *)malloc(num_digits + 1);
-    if (buffer == NULL) {
-        /* Handle memory allocation failure */
-        exit(EXIT_FAILURE);
-    }
+	if (buffer == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
 
-    /* Convert the unsigned integer to octal string */
-    sprintf(buffer, "%o", value);
+	sprintf(buffer, "%o", value);
 
-    return buffer;
+	return (buffer);
 }
 
-/** Function to convert unsigned integer to hexadecimal string */
-char *uint_to_hex_str(unsigned int value, int uppercase) {
-    /* Determine the maximum number of digits required */
-    int num_digits = snprintf(NULL, 0, uppercase ? "%X" : "%x", value);
+char *uint_to_hex_str(unsigned int value, int uppercase)
+{
+	int num_digits = snprintf(NULL, 0, uppercase ? "%X" : "%x", value);
+	char *buffer = (char *)malloc(num_digits + 1);
 
-    /* Allocate memory for the string, including space for the null terminator */
-    char *buffer = (char *)malloc(num_digits + 1);
-    if (buffer == NULL) {
-        /* Handle memory allocation failure */
-        exit(EXIT_FAILURE);
-    }
+	if (buffer == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
 
-    /* Convert the unsigned integer to hexadecimal string */
-    sprintf(buffer, uppercase ? "%X" : "%x", value);
+	sprintf(buffer, uppercase ? "%X" : "%x", value);
 
-    return buffer;
+	return (buffer);
 }
 
